1bedc9bc4dfd684f28ab1b92723811fdf7e476b0
George Hadjiyiannis <ghadjiyiannis@yahoo.com>
2019-11-22 22:27:23 +0100

    Finished implementation of ConnectedWorker.

--- HTTPLite/src/main/java/org/ghi/external/HTTPLite/ConnectedWorker.java
+++ HTTPLite/src/main/java/org/ghi/external/HTTPLite/ConnectedWorker.java
@@ -18,12 +18,12 @@ import rawhttp.core.body.FileBody;
 import rawhttp.core.body.StringBody;
 
 /**
+ * The ConnectedWorker is responsible for serving all the requests that come from
+ * a connection. It currently serves only GET and HEAD requests.
+ * It also respects the keep-alive flag.
  * 
- * 
- * 
- * 
- * The handling of the RawHttp requests and responses is based on the examples at
- * https://renatoathaydes.github.io/rawhttp/docs/low-level-api/
+ * The handling of the RawHttp requests and responses is based on the examples
+ * at https://renatoathaydes.github.io/rawhttp/docs/low-level-api/
  * 
  * @author George Hadjiyiannis
  *
@@ -36,8 +36,9 @@ public class ConnectedWorker implements Runnable {
 	private boolean keepalive;
 	private IFileUtil fileUtil;
 
-	public ConnectedWorker(IApplicationInjector injector, Logger logger, Socket clientSocket) throws ApplicationException {
-		
+	public ConnectedWorker(IApplicationInjector injector, Logger logger, Socket clientSocket)
+			throws ApplicationException {
+
 		// sanity check the inputs
 		if (null == injector)
 			throw new ApplicationException("Injector cannot be null!");
@@ -56,34 +57,33 @@ public class ConnectedWorker implements Runnable {
 	}
 
 	public void run() {
-		// TODO Auto-generated method stub
 		logger.debug("Connected...");
-		
+
 		// lets create a parser for the protocol
 		RawHttp http = new RawHttp();
-		
+
 		try {
 			// loop as we may be servicing multiple requests if keep-alive is set
-			while(true) {
-				
+			while (true) {
+
 				// serve a single request
 				serveSingleRequest(http);
-				
+
 				// check and abide by the keep-alive flag
 				if (!keepalive)
 					break;
 			}
-			
+
 		} catch (Exception e) {
-			
+
 			logger.error("Internal server error while processing request: ", e);
-			
+
 			// respond with 500 Internal Server Error
 			respondInternalServerError(http, e.getMessage());
-			
+
 		} finally {
 			logger.debug("Exiting connection...");
-			
+
 			// make sure we close the clientSocket no matter how we exit
 			try {
 				if (!clientSocket.isClosed())
@@ -108,34 +108,46 @@ public class ConnectedWorker implements Runnable {
 		} catch (IOException e) {
 			throw new ApplicationException("Error while parsing request: ", e);
 		}
-		
+
 		// check the keep-alive flag
 		keepalive = hasRequestedKeepalive(request);
-		
+
 		// check the requested method
 		String method = request.getMethod();
 		// we can only process GET and HEAD requests
 		if (method.contentEquals("GET") || method.equals("HEAD")) {
-			
+
 			// get the path, URL decoded and with the query string removed
 			String path = request.getUri().getPath();
 			
+			/*
+			 * I was planning to remove /../ sequences to make sure that one cannot climb out
+			 * of document root but it turns out RawHttp already takes care of that
+			 */
+			
+			logger.debug("Serving from path " + path);
+
 			// lets check the file out a bit
 			File file = fileUtil.getFileFromPath(path);
-			
+
 			// if it does not exists respond with 404 Not Found
-			if (!fileUtil.exists(file))
+			if (!fileUtil.exists(file)) {
 				respondNotFound(http);
-			
+				return;
+			}
+
 			// if it is not readable respond with 403 Forbidden
-			respondForbidden(http);
-			
+			if (!fileUtil.isReadable(file)) {
+				respondForbidden(http);
+				return;
+			}
+
 			// now send the response
 			if (method.equals("GET"))
 				respondOKWithContent(http, file);
-			else 
+			else
 				respondOKWithoutContent(http, file);
-			
+
 		} else {
 			// respond with 405 Method Not Allowed
 			respondMethodNotAllowed(http);
@@ -155,13 +167,27 @@ public class ConnectedWorker implements Runnable {
 	private void respondOKWithContent(RawHttp http, File file) throws ApplicationException {
 		// get the content type from the file itself
 		String contentType = fileUtil.getContentType(file);
+		long contentLength = fileUtil.getContentLength(file);
 		// get the last modified date
-		Date lastModifiedDate = fileUtil.getLastModifiedDate(file);
-		
-		// create the response
-		Optional<Date> lastModified = Optional.of(lastModifiedDate);
-		RawHttpResponse<?> response =  createBasicResponse(http, 200, "OK", contentType, lastModified);
-		response.withBody(new FileBody(file));
+		Date lastModified = fileUtil.getLastModifiedDate(file);
+
+		// get the date and time of the response
+		String dateString = RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC));
+
+		// if keep-alive is set we need to insert the header in the response
+		String connectionHeader = keepalive ? "Connection: keep-alive\r\n" : "";
+
+		// if we have a Last Modified date we need to insert it
+		String lastModifiedHeader = "Last-Modified: " + lastModified + "\r\n";
+
+		// construct the response
+		RawHttpResponse<?> response = http.parseResponse(
+				"HTTP/1.1 200 OK \r\n" + 
+				"Content-Type: " + contentType + "\r\n" + 
+				"Content-Length: " + contentLength + "\r\n" + 
+				"Server: RawHTTP\r\n" + "Date: " + dateString + "\r\n" + 
+				connectionHeader + 
+				lastModifiedHeader + "\r\n").withBody(new FileBody(file, contentType));
 		// send it
 		try {
 			response.writeTo(clientSocket.getOutputStream());
@@ -171,15 +197,30 @@ public class ConnectedWorker implements Runnable {
 	}
 
 	// respond with 200 OK (without content)
-	private void respondOKWithoutContent(RawHttp http, File file) throws ApplicationException  {
+	private void respondOKWithoutContent(RawHttp http, File file) throws ApplicationException {
 		// get the content type from the file itself
 		String contentType = fileUtil.getContentType(file);
+		long contentLength = 0; // we are not sending any content
 		// get the last modified date
-		Date lastModifiedDate = fileUtil.getLastModifiedDate(file);
-		
-		// create the response
-		Optional<Date> lastModified = Optional.of(lastModifiedDate);
-		RawHttpResponse<?> response =  createBasicResponse(http, 200, "OK", contentType, lastModified);
+		Date lastModified = fileUtil.getLastModifiedDate(file);
+
+		// get the date and time of the response
+		String dateString = RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC));
+
+		// if keep-alive is set we need to insert the header in the response
+		String connectionHeader = keepalive ? "Connection: keep-alive\r\n" : "";
+
+		// if we have a Last Modified date we need to insert it
+		String lastModifiedHeader = "Last-Modified: " + lastModified + "\r\n";
+
+		// construct the response
+		RawHttpResponse<?> response = http.parseResponse(
+				"HTTP/1.1 200 OK \r\n" + 
+				"Content-Type: " + contentType + "\r\n" + 
+				"Content-Length: " + contentLength + "\r\n" + 
+				"Server: RawHTTP\r\n" + "Date: " + dateString + "\r\n" + 
+				connectionHeader + 
+				lastModifiedHeader + "\r\n");
 		// send it
 		try {
 			response.writeTo(clientSocket.getOutputStream());
@@ -187,12 +228,26 @@ public class ConnectedWorker implements Runnable {
 			throw new ApplicationException("Error sending response: ", e);
 		}
 	}
-	
+
 	// respond with 403 Forbidden
 	private void respondForbidden(RawHttp http) throws ApplicationException {
-		// create the response
-		Optional<Date> lastModified = Optional.empty();
-		RawHttpResponse<?> response =  createBasicResponse(http, 403, "Forbidden", "plain/text", lastModified);
+		// get the content type from the file itself
+		String contentType = "plain/text";
+		long contentLength = 0; 
+
+		// get the date and time of the response
+		String dateString = RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC));
+
+		// if keep-alive is set we need to insert the header in the response
+		String connectionHeader = keepalive ? "Connection: keep-alive\r\n" : "";
+
+		// construct the response
+		RawHttpResponse<?> response = http.parseResponse(
+				"HTTP/1.1 403 Forbidden \r\n" + 
+				"Content-Type: " + contentType + "\r\n" + 
+				"Content-Length: " + contentLength + "\r\n" + 
+				"Server: RawHTTP\r\n" + "Date: " + dateString + "\r\n" + 
+				connectionHeader + "\r\n");
 		// send it
 		try {
 			response.writeTo(clientSocket.getOutputStream());
@@ -203,9 +258,23 @@ public class ConnectedWorker implements Runnable {
 
 	// respond with 404 Not Found
 	private void respondNotFound(RawHttp http) throws ApplicationException {
-		// create the response
-		Optional<Date> lastModified = Optional.empty();
-		RawHttpResponse<?> response =  createBasicResponse(http, 404, "Not Found", "plain/text", lastModified);
+		// get the content type from the file itself
+		String contentType = "plain/text";
+		long contentLength = 0; 
+
+		// get the date and time of the response
+		String dateString = RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC));
+
+		// if keep-alive is set we need to insert the header in the response
+		String connectionHeader = keepalive ? "Connection: keep-alive\r\n" : "";
+
+		// construct the response
+		RawHttpResponse<?> response = http.parseResponse(
+				"HTTP/1.1 404 Not Found \r\n" + 
+				"Content-Type: " + contentType + "\r\n" + 
+				"Content-Length: " + contentLength + "\r\n" + 
+				"Server: RawHTTP\r\n" + "Date: " + dateString + "\r\n" + 
+				connectionHeader + "\r\n");
 		// send it
 		try {
 			response.writeTo(clientSocket.getOutputStream());
@@ -216,9 +285,23 @@ public class ConnectedWorker implements Runnable {
 
 	// respond with 405 Method Not Allowed
 	private void respondMethodNotAllowed(RawHttp http) throws ApplicationException {
-		// create the response
-		Optional<Date> lastModified = Optional.empty();
-		RawHttpResponse<?> response =  createBasicResponse(http, 405, "Method Not Allowed", "plain/text", lastModified);
+		// get the content type from the file itself
+		String contentType = "plain/text";
+		long contentLength = 0; 
+
+		// get the date and time of the response
+		String dateString = RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC));
+
+		// if keep-alive is set we need to insert the header in the response
+		String connectionHeader = keepalive ? "Connection: keep-alive\r\n" : "";
+
+		// construct the response
+		RawHttpResponse<?> response = http.parseResponse(
+				"HTTP/1.1 405 Method Not Allowed \r\n" + 
+				"Content-Type: " + contentType + "\r\n" + 
+				"Content-Length: " + contentLength + "\r\n" + 
+				"Server: RawHTTP\r\n" + "Date: " + dateString + "\r\n" + 
+				connectionHeader + "\r\n");
 		// send it
 		try {
 			response.writeTo(clientSocket.getOutputStream());
@@ -226,42 +309,33 @@ public class ConnectedWorker implements Runnable {
 			throw new ApplicationException("Error sending response: ", e);
 		}
 	}
-	
+
 	// respond with 500 Internal Server Error
 	private void respondInternalServerError(RawHttp http, String msg) {
 		// the message can be null - fix it
-		msg = (null == msg)?"":msg;
-		
-		// create the response
-		Optional<Date> lastModified = Optional.empty();
-		RawHttpResponse<?> response =  createBasicResponse(http, 500, "Internal Server Error", "plain/text", lastModified);
-		response.withBody(new StringBody(msg));
+		msg = (null == msg) ? "" : msg;
+		// get the content type from the file itself
+		String contentType = "plain/text";
+		long contentLength = msg.length(); 
+
+		// get the date and time of the response
+		String dateString = RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC));
+
+		// if keep-alive is set we need to insert the header in the response
+		String connectionHeader = keepalive ? "Connection: keep-alive\r\n" : "";
+
+		// construct the response
+		RawHttpResponse<?> response = http.parseResponse(
+				"HTTP/1.1 500 Internal Server Error \r\n" + 
+				"Content-Type: " + contentType + "\r\n" + 
+				"Content-Length: " + contentLength + "\r\n" + 
+				"Server: RawHTTP\r\n" + "Date: " + dateString + "\r\n" + 
+				connectionHeader + "\r\n");
 		// send it
 		try {
 			response.writeTo(clientSocket.getOutputStream());
 		} catch (IOException e) {
-			logger.error("Unknown error while trying to responde with 500: ",  e);
-			// consume the exception - we were already sending a 500 and exiting...
+			// ignore - we were already sending a 500 and exiting!
 		}
-	}
-
-	private RawHttpResponse<?> createBasicResponse(RawHttp http, int status, String statusText,  String contentType, Optional<Date> lastModified) {
-		// get the date and time of the response
-		String dateString = RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC));
-		
-		// if keep-alive is set we need to insert the header in the response
-		String connectionHeader = keepalive?"Connection: keep-alive\r\n":"";
-		
-		// if we have a Last Modified date we need to insert it
-		String lastModifiedHeader = lastModified.isPresent()?"Last-Modified: " + lastModified.get().toString() + "\r\n":"";
-		
-		// construct the response
-		return http.parseResponse(
-			    "HTTP/1.1 " + status + " "+ statusText + "\r\n" +
-			    "Content-Type: " + contentType + "\r\n" +
-			    "Server: RawHTTP\r\n" +
-			    "Date: " + dateString + "\r\n" +
-			    connectionHeader +
-			    lastModifiedHeader);
 	}
 }
