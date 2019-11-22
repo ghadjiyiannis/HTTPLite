94fb30dea0969de2513b4621d583493304f394f6
George Hadjiyiannis <ghadjiyiannis@yahoo.com>
2019-11-22 09:49:32 +0100

    Initial Commit

---
+++ HTTPLite/src/main/java/org/ghi/external/HTTPLite/HTTPLite.java
@@ -0,0 +1,13 @@
+package org.ghi.external.HTTPLite;
+
+/**
+ * Hello world!
+ *
+ */
+public class HTTPLite 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
